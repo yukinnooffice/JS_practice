df299a01cb5c85a4c311f4e3bb05646e3bff81a2
YUKI MAEDA <yukimaeda@YUKInoMacBook-Air.local>
2020-11-26 02:04:20 +0900

    [finish]オブジェクト編9 mapを使ってみよう

--- js/object.js
+++ js/object.js
@@ -48,6 +48,18 @@
   });
 }
 
+//mapを使ってみよう
+{
+  const prices = [180, 190, 200];
+  
+  // const updatedPrices = prices.map((price) => {
+  //   return price + 20;
+  // });
+  const updatedPrices = prices.map(price => price + 20);
+
+  console.log(updatedPrices)
+}
+
 
 
 
