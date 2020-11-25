17521daf1dd088c9a82f429e781de14ef176b008
YUKI MAEDA <yukimaeda@YUKInoMacBook-Air.local>
2020-11-26 02:13:47 +0900

    [finish]オブジェクト編10 filterを使ってみよう

--- js/object.js
+++ js/object.js
@@ -60,6 +60,22 @@
   console.log(updatedPrices)
 }
 
+//filterを使ってみよう
+{
+  const numbers = [1, 4, 7, 8, 10];
+
+  // const evenNumbers = numbers.filter(number => {
+  //   if(number % 2 === 0){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
+  const evenNumbers = numbers.filter(number => number % 2 === 0);//省略
+
+
+  console.log(evenNumbers)
+}
 
 
 
