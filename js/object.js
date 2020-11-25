61460294a4bf4fe1a8aa761fa94ed43a973ff95f
YUKI MAEDA <yukimaeda@YUKInoMacBook-Air.local>
2020-11-26 03:19:54 +0900

    [finish]オブジェクト編15 変数を代入してみよう

--- js/object.js
+++ js/object.js
@@ -145,3 +145,19 @@
   ];
   console.log(points[1.].y);
 }
+
+//変数を代入してみよう
+
+{
+  // let x = 1;
+  // let y = x;
+  // x = 5;
+  // console.log(x); //5
+  // console.log(x); //1
+  let x = [1, 2];
+  let y = x;
+  x[0] = 5;
+  console.log(x); //[5,2]
+  console.log(y); //[1,2]
+
+}
