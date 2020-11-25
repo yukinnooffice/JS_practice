254e91e8e74631c3b70c0434af535bf25c7871ab
YUKI MAEDA <yukimaeda@YUKInoMacBook-Air.local>
2020-11-26 03:02:58 +0900

    [finish]オブジェクト編13 オブジェクトを操作してみよう

--- js/object.js
+++ js/object.js
@@ -106,4 +106,23 @@
   console.log(point);
   };
 
+  //オブジェクトを操作してみよう
+
+{ 
+  const otherProps = {
+    r: 4,
+    color: 'red'
+  };
+  const point = {
+    x:100,
+    y: 180,
+    ...otherProps,
+  };
+  console.log(point);
+
+  const{x, r, ...othrers} = point;
+  console.log(x);
+  console.log(r);
+  console.log(othrers);
+}
 
