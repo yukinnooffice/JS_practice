45dccbe886c6f67b2ad7a8fe18e7a110ae3e7ad9
YUKI MAEDA <yukimaeda@YUKInoMacBook-Air.local>
2020-11-29 01:18:11 +0900

    [finish]オブジェクト編20 ランダムな整数値を作ってみよう

--- js/object.js
+++ js/object.js
@@ -219,3 +219,17 @@ console.log(y); //[1,2]
 
   console.log(Math.random());
 }
+// 20 ランダムな整数値を作ってみよう
+  {
+    // console.log(Math.random());
+  
+    // 0, 1, 2
+    // Math.floor(Math.random() * 3)
+    // 0, ..., n
+    // Math.floor(Math.random() * (n + 1))
+    // min, ..., max
+    // Math.floor(Math.random() * (max + 1 - min)) + min
+  
+    console.log(Math.floor(Math.random() * 6) + 1);
+  }
+  
