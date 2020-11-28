88a489c9609003a6b56189fad1075cd142b9bd9a
YUKI MAEDA <yukimaeda@YUKInoMacBook-Air.local>
2020-11-28 22:47:40 +0900

    [finish]オブジェクト編18 join()､split()を使ってみよう

--- js/object.js
+++ js/object.js
@@ -182,3 +182,18 @@ console.log(y); //[1,2]
 
   console.log(str[1]);
 }
+
+//join()､split()を使ってみよう
+{
+  const d = [2019, 11 , 14];
+
+  // console.log(d.join('/'));
+  // console.log(d.join(''));
+
+  const t = '17:08:24';
+  // console.log(t.split(':'));
+  const [hour, minute , second] = t.split(':');
+  console.log(hour)
+  console.log(minute)
+  console.log(second)
+}
